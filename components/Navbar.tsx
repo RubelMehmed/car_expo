c5124f03dd8dfd0282f8e4918fcdf829eb1ba3a3
RubelMehmed <rubel.49@geography-juniv.edu.bd>
2023-12-01 20:46:04 +0600

    feat: custom search combobox

---
+++ components/Navbar.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
+import { CustomButton } from ".";
+
+const Navbar = () => {
+  return (
+    <header className="w-full absolute z-10">
+      <nav className="max-w-[440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent">
+        <Link href="/" className="flex justify-center items-center">
+          <Image
+            src="/logo.png"
+            alt="CAr Expo Logo"
+            width={118}
+            height={18}
+            className="object-contain"
+          />
+        </Link>
+
+        <CustomButton
+          title="Sign In"
+          btnType="button"
+          containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]"
+        />
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
